af27f4bb44c9b9f13e5a7285c8e1177bd9810207
Sushmita S Warik <sushmitaswarik@gmail.com>
2026-02-20 18:36:08 +0530

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+// import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+// Bootstrap css imported
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Bootstrap icon imported
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  // <StrictMode>
+    <App />
+  // </StrictMode>,
+)
